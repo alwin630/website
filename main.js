c334d8e4f402c20f43d119ce930cd571247e18c8
alwin630 <alwin630@gmail.com>
2023-11-07 09:05:46 -0500

    show 80% of working weight on submit

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ function calculate_ul1() {
     var roundto = 2.5;
 
     // Perform a calculation (for example, double the input)
+    var eightypercent = 2.5 * Math.floor((userInput * .8) / 2.5)
     var forty_percent_or_bar = Math.max(2.5 * Math.floor((userInput * .4) / 2.5), 45);
     var fifty_five_percent = Math.max(2.5 * Math.floor((userInput * .55) / 2.5), 45);
     var seventy_percent = Math.max(2.5 * Math.floor((userInput * .7) / 2.5), 45);
     var eighty_five_percent = Math.max(2.5 * Math.floor((userInput * .85) / 2.5), 45);
 
     // Display the result
+    document.getElementById("li80percent").textContent = "80% of " + userInput + " = " + eightypercent + " lbs";
     document.getElementById("li1").textContent = forty_percent_or_bar + " lbs";
-    document.getElementById("li2").textContent = fifty_five_percent + " test git";
+    document.getElementById("li2").textContent = fifty_five_percent + " lbs";
     document.getElementById("li3").textContent = seventy_percent + " lbs";
     document.getElementById("li4").textContent = eighty_five_percent + " lbs";
 }
