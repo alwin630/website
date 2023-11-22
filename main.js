283374a6fbe973effd9a77c97048d267bcd20f94
alwin630 <alwin630@gmail.com>
2023-11-22 15:24:57 -0500

    add indents

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function calculate_ul1(input) {
       var userInput = document.getElementById("userWorkingWeight").value * .8;
       break;
   }
+
   // Perform a calculation (for example, double the input)
   var eightypercent = 2.5 * Math.floor((userInput * .8) / 2.5)
   var forty_percent_or_bar = Math.max(2.5 * Math.floor((userInput * .4) / 2.5), 45);
@@ -24,12 +25,12 @@ function calculate_ul1(input) {
   var workingweight_result = writeResultString(calculatePlatesForWeight(userInput));
 
   // Display the result
-  document.getElementById("li_eighty_p").textContent = "80% of " + userInput + " = " + eightypercent + " lbs " + eightypercent_result;
-  document.getElementById("li1").textContent = "5: " + forty_percent_or_bar + " lbs " + forty_percent_or_bar_result;
-  document.getElementById("li2").textContent = "4: " + fifty_five_percent + " lbs " + fifty_five_percent_result;
-  document.getElementById("li3").textContent = "3: " + seventy_percent + " lbs " + seventy_percent_result;
-  document.getElementById("li4").textContent = "2: " + eighty_five_percent + " lbs " + eighty_five_percent_result;
-  document.getElementById("li5").textContent = "working: " + userInput + " lbs " + workingweight_result;
+  document.getElementById("li_eighty_p").innerHTML = "80% of " + userInput + " = " + eightypercent + " lbs <span style='padding-left:25px;'>" + eightypercent_result + "</span>";
+  document.getElementById("li1").innerHTML = "5: " + forty_percent_or_bar + " lbs <span style='padding-left:75px;'>" + forty_percent_or_bar_result + "</span>";
+  document.getElementById("li2").innerHTML = "4: " + fifty_five_percent + " lbs <span style='padding-left:75px;'>" + fifty_five_percent_result + "</span>";
+  document.getElementById("li3").innerHTML = "3: " + seventy_percent + " lbs <span style='padding-left:75px;'>" + seventy_percent_result + "</span>";
+  document.getElementById("li4").innerHTML = "2: " + eighty_five_percent + " lbs <span style='padding-left:75px;'>" + eighty_five_percent_result + "</span>";
+  document.getElementById("li5").innerHTML = "working: " + userInput + " lbs <span style='padding-left:50px;'>" + workingweight_result + "</span>";
 }
 
 function writeResultString(usedPlates) {
