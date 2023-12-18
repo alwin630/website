8a8c99d3fcae4850cbd747d92c513ca5fdbe4792
alwin630 <Alwin630@gmail.com>
2023-12-17 20:14:50 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,28 +1,15 @@
 function countdown() {
-    // Get the current date and time
     var now = new Date();
-
-    // Create a new date for tomorrow
     var tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    tomorrow.setHours(8, 15, 0, 0); // Set to 8:15 AM
 
-    // Set the time to 8:15 AM EST
-    // Note: JavaScript works in the local time zone, so adjust the hours accordingly to match EST
-    // If your local time zone is EST, you can directly set it to 8:15 AM
-    tomorrow.setHours(8, 15, 0, 0);
-
-    // Calculate the difference in milliseconds
     var diff = tomorrow - now;
-
-    // Convert the difference to hours, minutes, and seconds
     var hours = Math.floor(diff / 1000 / 60 / 60);
     var minutes = Math.floor(diff / 1000 / 60) % 60;
-    var seconds = Math.floor(diff / 1000) %     // Get the paragraph element by its ID
-    var countdownElement = document.getElementById("countdownTimer");
+    var seconds = Math.floor(diff / 1000) % 60;
 
-    // Update the content of the paragraph
-    countdownElement.textContent = hours + " hours " + minutes + " minutes " + seconds + " seconds ";
+    document.getElementById("countdownTimer").textContent = hours + " hours " + minutes + " minutes " + seconds + " seconds ";
 
-    // Update the countdown every second
     setTimeout(countdown, 1000);
 }
 
